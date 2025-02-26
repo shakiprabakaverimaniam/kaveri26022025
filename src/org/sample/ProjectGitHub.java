d089b03912109c27dbed0ae47e5e504a4cfbe70b
shakiprabakaverimaniam <shaki16102001@gmail.com>
2025-02-26 18:14:16 +0530

    Hi yaazhan

--- src/org/sample/ProjectGitHub.java
+++ src/org/sample/ProjectGitHub.java
@@ -1,5 +1,12 @@
 package org.sample;
 
 public class ProjectGitHub {
+private void test1() {
+	System.out.println("Testcase1");
 
+}
+public static void main(String[] args) {
+	ProjectGitHub p = new ProjectGitHub();
+	p.test1();
+}
 }
